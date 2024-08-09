d1bf40dd846e5b046377eef546eecccd34de4cd9
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2024-08-09 11:47:55 -0400

    MariaDB Compatiblity

--- app/xml_cdr/app_defaults.php
+++ app/xml_cdr/app_defaults.php
@@ -39,6 +39,14 @@
 			}
 		}
 		*/
+
+       //create a view for xml details
+               $database = new database;
+               $database->execute("DROP VIEW h;", null);
+               $sql = "CREATE VIEW h  AS ( SELECT 0 AS s_id, 1 AS s_start, 0 AS s_end, 1 AS s_hour UNION SELECT 1 AS s_id, 2 AS s_start, 1 AS s_end, 1 AS s_hour UNION SELECT 2 AS s_id, 3 AS s_start, 2 AS s_end, 1 AS s_hour UNION SELECT 3 AS s_id, 4 AS s_start, 3 AS s_end, 1 AS s_hour UNION SELECT 4 AS s_id, 5 AS s_start, 4 AS s_end, 1 AS s_hour UNION SELECT 5 AS s_id, 6 AS s_start, 5 AS s_end, 1 AS s_hour UNION SELECT 6 AS s_id, 7 AS s_start, 6 AS s_end, 1 AS s_hour UNION SELECT 7 AS s_id, 8 AS s_start, 7 AS s_end, 1 AS s_hour UNION SELECT 8 AS s_id, 9 AS s_start, 8 AS s_end, 1 AS s_hour UNION SELECT 9 AS s_id, 10 AS s_start, 9 AS s_end, 1 AS s_hour UNION SELECT 10 AS s_id, 11 AS s_start, 10 AS s_end, 1 AS s_hour UNION SELECT 11 AS s_id, 12 AS s_start, 11 AS s_end, 1 AS s_hour UNION SELECT 12 AS s_id, 13 AS s_start, 12 AS s_end, 1 AS s_hour UNION SELECT 13 AS s_id, 14 AS s_start, 13 AS s_end, 1 AS s_hour UNION SELECT 14 AS s_id, 15 AS s_start, 14 AS s_end, 1 AS s_hour UNION SELECT 15 AS s_id, 16 AS s_start, 15 AS s_end, 1 AS s_hour UNION SELECT 16 AS s_id, 17 AS s_start, 16 AS s_end, 1 AS s_hour UNION SELECT 17 AS s_id, 18 AS s_start, 17 AS s_end, 1 AS s_hour UNION SELECT 18 AS s_id, 19 AS s_start, 18 AS s_end, 1 AS s_hour UNION SELECT 19 AS s_id, 20 AS s_start, 19 AS s_end, 1 AS s_hour UNION SELECT 20 AS s_id, 21 AS s_start, 20 AS s_end, 1 AS s_hour UNION SELECT 21 AS s_id, 22 AS s_start, 21 AS s_end, 1 AS s_hour UNION SELECT 22 AS s_id, 23 AS s_start, 22 AS s_end, 1 AS s_hour UNION SELECT 23 AS s_id, 24 AS s_start, 23 AS s_end, 1 AS s_hour UNION SELECT 25 AS s_id, 24 AS s_start, 0 AS s_end, 24 AS s_hour UNION SELECT 26 AS s_id, 168 AS s_start, 0 AS s_end, 168 AS s_hour UNION SELECT 27 AS s_id, 720 AS s_start, 0 AS s_end, 720 AS s_hour ); \n";
+               $database = new database;
+               $database->execute($sql, null);
+               unset($sql);
 	}
 
 ?>
